1aaa7ce1ba6723d5be941ff5664e70c0e9578410
ThanasitAio <98828237+ThanasitAio@users.noreply.github.com>
2022-02-11 00:30:01 +0700

    Create head.php

---
+++ inc/head.php
@@ -0,0 +1,17 @@
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css">
+
+<link rel="stylesheet" href="css/footer.css">
+<link rel="stylesheet" href="css/menu_footer.css">
+<link rel="stylesheet" href="css/menu.css">
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+<script src="//cdn.jsdelivr.net/npm/sweetalert2@11"></script>
+
+<link rel="stylesheet" href="../css/footer.css">
+<link rel="stylesheet" href="../css/menu_footer.css">
+<link rel="stylesheet" href="../css/menu.css">
+<link rel="stylesheet" href="../css/Sidebar.css">
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+<script src="//cdn.jsdelivr.net/npm/sweetalert2@11"></script>
+
+
+
