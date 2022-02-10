c3309f6bb2a86614e53dd58152640a4e0f2c71aa
ThanasitAio <98828237+ThanasitAio@users.noreply.github.com>
2022-02-11 00:26:27 +0700

    Create admin.php

---
+++ Admin/admin.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Admin</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css">
+    <?php include '../inc/head.php'; ?>
+
+
+
+</head>
+<body>
+    <?php
+     session_start();
+     require_once "../db.php";
+     
+    ?>
+     <!--<?php include '../inc/menu.php'; ?>-->
+     <?php include '../inc/Sidebar.php'; ?>
+
+</body>
+</html>
